b4989e522f18da58dc0b06a631e509a58fa2b127
Replit user <>
2022-11-17 22:15:03 +0530

    for-replit run

--- src/app/FetchApi/Api.js
+++ src/app/FetchApi/Api.js
@@ -4,7 +4,7 @@ const Api = axios.create({
     // baseURL: "http://15.206.124.89/api",
     // baseURL: "http://18.214.99.164/api",
     // baseURL: "https://asc.apptology.in/api/",
-    baseURL: "http://54.221.37.44/api",
+    baseURL: "https://asc.apptology.in/api",
 });
 
 export default Api;
